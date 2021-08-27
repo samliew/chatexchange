a3b43ce138ac14422aacd99645bb1fa77b6afa4f
Oleg Valter <o.a.philippov@gmail.com>
2021-08-27 13:57:43 +0300

    added instance properties for the LoginError class

--- src/Exceptions/LoginError.ts
+++ src/Exceptions/LoginError.ts
@@ -1,5 +1,19 @@
+import { CookieJar } from "tough-cookie";
 import ChatExchangeError from "./ChatExchangeError";
 
-export class LoginError extends ChatExchangeError {}
+interface LoginErrorInfo {
+    email?: string;
+    password?: string;
+    cookie?: string | CookieJar.Serialized;
+}
+
+export interface LoginError extends LoginErrorInfo {}
+
+export class LoginError extends ChatExchangeError {
+    constructor(message: string, info: LoginErrorInfo) {
+        super(message);
+        Object.assign(this, info);
+    }
+}
 
 export default LoginError;
